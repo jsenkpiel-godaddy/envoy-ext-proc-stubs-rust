42c1d2caa332ab47476e33626f8811e1e3e9039a
Greg Brail <gbrail@users.noreply.github.com>
2023-01-10 16:47:05 -0800

    Use the bytes crate.

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut proto_config = prost_build::Config::new();
+    proto_config.bytes(["."]);
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .compile(
+        .compile_with_config(
+            proto_config,
             &["data-plane-api/envoy/service/ext_proc/v3/external_processor.proto"],
             &["data-plane-api/", "udpa/", "protoc-gen-validate/", "xds/"],
         )?;
